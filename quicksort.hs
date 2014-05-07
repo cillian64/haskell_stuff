0c1dd554c86bf787a63303f412c36a31b368aeca
David Turner <david.w.turner@cl.cam.ac.uk>
2014-05-07 15:23:59 +0100

    Moved helper functions to `where' scoping.

--- quicksort.hs
+++ quicksort.hs
@@ -2,10 +2,9 @@ qs :: (Ord a) => [a] -> [a]
 qs [] = []
 qs [i] = [i]
 qs (p:l) = qs (left l p) ++ p : qs (right l p)
-
-left :: (Ord a) => [a] -> a -> [a]
-left l p = [x | x <- l, x < p]
-
-right :: (Ord a) => [a] -> a -> [a]
-right l p = [x | x <- l, x >= p]
+    where left :: (Ord a) => [a] -> a -> [a]
+          left l p = [x | x <- l, x < p]
+          
+          right :: (Ord a) => [a] -> a -> [a]
+          right l p = [x | x <- l, x >= p]
 
