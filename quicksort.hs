b99848b70162efa6b33bacd911458148d8608bf5
David Turner <cillian64@googlemail.com>
2014-05-07 19:05:29 +0100

    Converted quicksort helper functions to variables

--- quicksort.hs
+++ quicksort.hs
@@ -2,28 +2,18 @@
 qs :: (Ord a) => [a] -> [a]
 qs [] = []
 qs [i] = [i]
-qs (p:l) = qs (left l p) ++ p : qs (right l p)
-    where left :: (Ord a) => [a] -> a -> [a]
-          left l p = [x | x <- l, x < p]
-          
-          right :: (Ord a) => [a] -> a -> [a]
-          right l p = [x | x <- l, x >= p]
+qs (p:l) = (qs left) ++ p : (qs right)
+    where left = [x | x <- l, x < p]
+          right = [x | x <- l, x >= p]
 
 -- Uses a general pivot function which could be clever-ified
 -- Also glomps all equal-to-pivot elements at each level (middle)
 qs' :: (Ord a) => [a] -> [a]
 qs' [] = []
 qs' [i] = [i]
-qs' l = qs' (left l (pivot l)) ++ middle l (pivot l) ++ qs' (right l (pivot l))
-    where left :: (Ord a) => [a] -> a -> [a]
-          left l p = [x | x <- l, x < p]
-
-          middle :: (Eq a) => [a] -> a -> [a]
-          middle l p = [x | x <- l, x == p]
-
-          right :: (Ord a) => [a] -> a -> [a]
-          right l p = [x | x <- l, x > p]
-
-          pivot :: [a] -> a
-          pivot l = head l -- for now!
+qs' l = (qs' left) ++ middle ++ (qs' right)
+    where left = [x | x <- l, x < p]
+          middle = [x | x <- l, x == p]
+          right = [x | x <- l, x > p]
+          p = head l -- for now!
 
