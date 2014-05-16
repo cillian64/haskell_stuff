a399ccdda5281736321f7de83f96a2e10e20bf7a
David Turner <david.w.turner@cl.cam.ac.uk>
2014-05-16 16:03:00 +0100

    splitAt instead of split_even and split_odd in ms

--- mergesort.hs
+++ mergesort.hs
@@ -1,18 +1,10 @@
 mergesort :: (Ord a) => [a] -> [a]
 mergesort [] = []
 mergesort [a] = [a]
-mergesort a = merge (mergesort (split_odd a)) (mergesort (split_even a))
+mergesort a = merge (mergesort left) (mergesort right)
     where
-        split_odd :: [a] -> [a] -- Take just the odd elements
-        split_odd [] = []
-        split_odd [a] = []
-        split_odd (_:a:as) = a : split_odd as
-        
-        split_even :: [a] -> [a] -- Take just the even elements
-        split_even [] = []
-        split_even [a] = [a]
-        split_even (a:_:as) = a : split_even as
-        
+        (left, right) = splitAt (round $ (fromIntegral $ length a)/2 ) a
+       
         merge :: (Ord a) => [a] -> [a] -> [a]
         merge [] [] = []
         merge a [] = a
